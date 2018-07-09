c29a8283408483bfaf52daf3d6413ba1a712e72a
Yumainaura <yuma.inaura@gmail.com>
2018-07-10 07:41:45 +0900

    Fix ruby refinments example

--- ruby/script/refinments.rb
+++ ruby/script/refinments.rb
@@ -7,8 +7,20 @@ module RefineStringModule
 end
 
 class SomeClass
+  using RefineStringModule
+
+  def self.some_method
+    puts '123456789'.first_5_characters # 12345
+  end
 end
 
-using RefineStringModule
+class OtherClass
+  def self.some_method
+     puts '123456789'.first_5_characters
+  end
+end
 
-puts '123456789'.first_5_characters # 12345
+SomeClass.some_method
+
+# OtherClass.some_method # `some_method': undefined method `first_5_characters' for "123456789":String (NoMethodError)
+
