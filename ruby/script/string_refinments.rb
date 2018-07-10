73e52861ca6f3f891212444d513fb1c1c4db5cf4
Yumainaura <yuma.inaura@gmail.com>
2018-07-10 20:42:56 +0900

    Update ruby refinements example

--- ruby/script/string_refinments.rb
+++ ruby/script/string_refinments.rb
@@ -1,7 +1,7 @@
 module MyRefineStringModule
   refine Integer do
     def to_s
-      'overwrited by refinments'
+      'overwritten by refinments'
     end
   end
 end
@@ -9,7 +9,19 @@ end
 class SomeClass
   using MyRefineStringModule
   
-  puts 1.to_s # overwrited by refinements
+  puts 1.to_s # overwritten by refinements
+
+  def some_instance_method
+    1.to_s
+  end
+  
+  def self.some_class_method
+    1.to_s
+  end
 end
+
+puts SomeClass.new.some_instance_method # overwritten by refinments
+
+puts SomeClass.some_class_method # overwritten by refinments
 
 puts 1.to_s # 1
