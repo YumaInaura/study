81c1c7dea2fdf15e3a725d5cc719854326ce8426
Yumainaura <yuma.inaura@gmail.com>
2018-07-15 18:07:20 +0900

    Add rspec check_tags_existence_in_configure example

--- ruby/gem/rspec/tags.rb
+++ ruby/gem/rspec/tags.rb
@@ -1,27 +1,3 @@
-# Run example
-# 
-# rspec tags_spec.rb_spec.rb --tag=some_tag
-# some_tag specified
-#
-# rspec tags_spec.rb_spec.rb
-# some_tag does not exist
-
-RSpec.configure do |config|
-  if config.filter.rules[:some_tag]
-    puts '"some_tag" specified'
-  else
-    puts '"some_tag" not specified'
-  end
-
-  config.before :all, some_tag: false do
-    puts 'before all when some_tag: false'
-  end
-  
-  config.before :example, some_tag: false do
-    puts 'before example when some_tag: false'
-  end
-end
-
 RSpec.describe do
   # Run cases
   # rspec -fd rspec/tags_spec.rb.rb 
@@ -29,7 +5,7 @@ RSpec.describe do
   # Not run cases
   # rspec -fd rspec/tags_spec.rb.rb  --tag=~some_tag ( exclude some_tag )
   # rspec -fd rspec/tags_spec.rb.rb  --tag=another_tag
-  describe 'when run some_tag: true', some_tag: true do
+  describe 'run when some_tag: true', some_tag: true do
     subject { true }
     it { is_expected.to be true }
     it { is_expected.to be true }
@@ -41,7 +17,7 @@ RSpec.describe do
   # Not run cases
   # rspec -fd rspec/tags_spec.rb.rb --tag=some_tag 
   # rspec -fd rspec/tags_spec.rb.rb --tag=another_tag
-  describe 'when run some_tag: false', some_tag: false do
+  describe 'run when some_tag: false', some_tag: false do
     subject { true }
     it { is_expected.to be true }
     it { is_expected.to be true }
