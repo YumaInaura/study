bf8ff61c27e8c954e03df7943ab4fb81fcca3a4f
YumaInaura <yinaura@aiming-inc.com>
2018-07-20 13:15:47 +0900

    Add shell newline_command example

---
+++ shell/newline_command.sh
@@ -0,0 +1,15 @@
+echo A \
+     B \
+     C
+
+echo D \
+     E \ 
+     F
+
+# A B C
+# D E
+# shell/newline_command.sh: line 7: F: command not found
+
+# What's wrong !?
+# You can see space after back slack in "E" line
+
